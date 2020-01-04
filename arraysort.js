705b4cfe9db3e7e68d2fa5494d970fd9f99f8e89
Gryc <nakazibwe24grace@gmail.com>
2020-01-04 13:58:39 -0800

    This is my third commit

--- arraysort.js
+++ arraysort.js
@@ -5,14 +5,14 @@ let  unSortedArray = [2,0,6,5,1,7,'z','a'];
 
 console.log(unSortedArray);
  function sortArray(unSortedArray){
-   let  evens = " ";
-   let odds = " ";
-   let chars = " ";
+   let  evens = [];
+   let odds = [];
+   let chars = [];
    for (let i = 0; i < unSortedArray.length; i++) {
      if(unSortedArray[i]%2==0){
       evens  += unSortedArray[i] ;
      }
-     else if(unSortedArray[i]%2!=0){
+     else if(unSortedArray[i]%2>=1){
           odds += unSortedArray[i]  ;
      }
      else{
@@ -20,17 +20,9 @@ console.log(unSortedArray);
      }
   
    }
-   return "evens" + evens + "odds" + odds + "chars" + chars ; 
+   return "evens :" + [evens] + "  odds :" + [odds] + "  chars :" + [chars] ; 
 }
 console.log(sortArray(unSortedArray));
    
   
-    // let selectedVowels = "" ;
-    // let dupe = 0;
-    
-    // let w = ("nakazibwe");
-    // w = w.split('');
-    // const vowels = ['a','e','i','o','u']
-    // function pickVowels(w) {
-    //     for(i=0; i < w.length; i++){
-    //       if (vowels.includes(w[i])) {
+   
